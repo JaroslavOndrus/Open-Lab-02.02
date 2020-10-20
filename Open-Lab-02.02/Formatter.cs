025a6e4e7c089be974769881349113de7ceb6cf7
Jaroslav Ondruš <jaroslavondrus05@gmail.com>
2020-10-20 09:03:15 +0200

    Using method getLenght for [] numbers.

--- Open-Lab-02.02/Formatter.cs
+++ Open-Lab-02.02/Formatter.cs
@@ -6,7 +6,10 @@ namespace Open_Lab_02._02
     {
         public void Format(double[] numbers)
         {
-            Console.Write(string.Format("{0:0.00} {1:0.00} {2:0.00} {3:0.00} {4:0.00}", numbers[0], numbers[1], numbers[2], numbers[3], numbers[4]));
+            for (int x = 0; x < numbers.GetLength(0); x++)
+            {
+                Console.Write(string.Format( "{0:0.00} ", numbers[x]));
+            }
         }
     }
 }
